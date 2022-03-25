d144ec8d484c788db3a4af24ffe37b3017ed7077
Marce <Marce@DESKTOP-96DOL3D>
2022-03-25 14:50:16 +0100

    Add project files.

---
+++ R7R8MW_HFT_2021222.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R7R8MW_HFT_2021222.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
