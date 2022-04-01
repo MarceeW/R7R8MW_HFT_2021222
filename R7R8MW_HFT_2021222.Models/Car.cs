4c40c004f951af7fd959261eb547377e42f071f4
marcellkadar@stud.uni-obuda.hu <marcellkadar@stud.uni-obuda.hu>
2022-04-01 15:39:22 +0200

    EntityFramework nuget packages installed

--- R7R8MW_HFT_2021222.Models/Car.cs
+++ R7R8MW_HFT_2021222.Models/Car.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace R7R8MW_HFT_2021222.Models
 {
     internal class Car
     {
+        [Key]
+        []
+        public int CarId { get; set; }
+        public Brand Brand { get; set; }
+        public RentEvent RentEvent { get; set; }
     }
 }
