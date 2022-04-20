fc4b65f34e6ab076671c001be957ca339643c052
marcellkadar@stud.uni-obuda.hu <marcellkadar@stud.uni-obuda.hu>
2022-04-20 13:06:59 +0200

    MovieLogic defined

---
+++ R7R8MW_HFT_2021222.Logic/Interfaces/IMovieLogic.cs
@@ -0,0 +1,17 @@
+﻿using R7R8MW_HFT_2021222.Models;
+using System.Linq;
+
+namespace R7R8MW_HFT_2021222.Logic
+{
+    public interface IMovieLogic
+    {
+        void Create(Movie entity);
+        void Delete(int id);
+        Movie LargestIncome();
+        Movie Oldest();
+        Movie Read(int id);
+        IQueryable<Movie> ReadAll();
+        Movie TopRating();
+        void Update(Movie entity);
+    }
+}
--- R7R8MW_HFT_2021222.Logic/Models/MovieLogic.cs
+++ R7R8MW_HFT_2021222.Logic/Models/MovieLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using R7R8MW_HFT_2021222.Models;
+using R7R8MW_HFT_2021222.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,68 @@ using System.Threading.Tasks;
 
 namespace R7R8MW_HFT_2021222.Logic
 {
-    internal class MovieLogic
+    public class MovieLogic : IMovieLogic
     {
+        MovieRepository repository;
+
+        public MovieLogic(MovieRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Create(Movie entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException();
+
+            repository.Create(entity);
+        }
+
+        public void Delete(int id)
+        {
+            if (id < 0)
+                throw new KeyNotFoundException();
+
+            repository.Delete(id);
+        }
+
+        public Movie Read(int id)
+        {
+            if (id < 0)
+                throw new KeyNotFoundException();
+
+            return repository.Read(id);
+        }
+
+        public IQueryable<Movie> ReadAll()
+        {
+            return repository.ReadAll();
+        }
+
+        public void Update(Movie entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException();
+
+            repository.Update(entity);
+        }
+        public Movie LargestIncome()
+        {
+            return (from x in repository.ReadAll()
+                    orderby x.Income
+                    select x).FirstOrDefault();
+        }
+        public Movie Oldest()
+        {
+            return (from x in repository.ReadAll()
+                    orderby x.Release
+                    select x).FirstOrDefault();
+        }
+        public Movie TopRating()
+        {
+            return (from x in repository.ReadAll()
+                    orderby x.Rating descending
+                    select x).FirstOrDefault();
+        }
     }
 }
