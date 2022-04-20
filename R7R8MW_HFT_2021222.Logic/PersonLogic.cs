c39adae1ee555c8cf7863f1e2c68a5ec25fd4f8d
marcellkadar@stud.uni-obuda.hu <marcellkadar@stud.uni-obuda.hu>
2022-04-20 11:50:07 +0200

    Logic classes created

---
+++ R7R8MW_HFT_2021222.Logic/PersonLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R7R8MW_HFT_2021222.Logic
+{
+    internal class PersonLogic
+    {
+    }
+}
