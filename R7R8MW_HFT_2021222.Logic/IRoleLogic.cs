e2a5b8cec21d7603d34d93eb0851fc4bca495797
marcellkadar@stud.uni-obuda.hu <marcellkadar@stud.uni-obuda.hu>
2022-04-20 12:50:09 +0200

    RoleLogic defined

---
+++ R7R8MW_HFT_2021222.Logic/IRoleLogic.cs
@@ -0,0 +1,15 @@
+﻿using R7R8MW_HFT_2021222.Models;
+using System.Linq;
+
+namespace R7R8MW_HFT_2021222.Logic
+{
+    public interface IRoleLogic
+    {
+        void Create(Role entity);
+        void Delete(int id);
+        string GetMostCommonRoleName();
+        Role Read(int id);
+        IQueryable<Role> ReadAll();
+        void Update(Role entity);
+    }
+}
--- R7R8MW_HFT_2021222.Logic/RoleLogic.cs
+++ R7R8MW_HFT_2021222.Logic/RoleLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using R7R8MW_HFT_2021222.Models;
+using R7R8MW_HFT_2021222.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,53 @@ using System.Threading.Tasks;
 
 namespace R7R8MW_HFT_2021222.Logic
 {
-    internal class RoleLogic
+    public class RoleLogic : IRoleLogic
     {
+        IRepository<Role> roleRepository;
+        public RoleLogic(IRepository<Role> rp)
+        {
+            roleRepository = rp;
+        }
+        public void Create(Role entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            if (id < 0)
+                throw new KeyNotFoundException();
+
+            roleRepository.Delete(id);
+        }
+
+        public Role Read(int id)
+        {
+            if (id < 0)
+                throw new KeyNotFoundException();
+
+            return roleRepository.Read(id);
+        }
+
+        public IQueryable<Role> ReadAll()
+        {
+            return roleRepository.ReadAll();
+        }
+
+        public void Update(Role entity)
+        {
+            if (entity == null)
+                throw new NullReferenceException();
+
+            roleRepository.Update(entity);
+        }
+        public string GetMostCommonRoleName()
+        {
+            return (from x in roleRepository.ReadAll()
+                    group x by x.RoleName into roles
+                    orderby roles.Count() descending
+                    select roles.Key).FirstOrDefault();
+
+        }
     }
 }
